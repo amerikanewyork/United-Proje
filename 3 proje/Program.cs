b814a28c8adbcefa6fb6fa96c540ab42342048bb
sa <halil@DESKTOP-TSHLOIG>
2023-07-08 09:41:24 +0300

    Add project files.

---
+++ 3 proje/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_proje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Başlıcağın değer gir");
+            int değer1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Finish değer gir");
+            int değer2 = Convert.ToInt32(Console.ReadLine());
+
+            int[] değerler = Değerüret(değer1, değer2);
+        }
+        public static int[] Değerüret(int değer1 , int değer2)
+        {
+            Random yuhlar = new Random();
+            int[] sayılsal = new int[10];
+            for (int i = 0; i < sayılsal.Length; i++)
+            {
+                sayılsal[i] = yuhlar.Next(değer1, değer2 + 1);
+            }
+
+            return sayılsal;
+        }
+        public static void bilinmiyor(int[] bilinmiyom)
+        {
+            foreach(int kişibaşı in bilinmiyom)
+            {
+                Console.WriteLine(kişibaşı+ " ");
+            }
+
+        }
+     
+        }
+    }
+}
---
+++ United Proje/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace United_Proje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int j = 1; j <= 10; j++) 
+            {
+                for (int i = 1; i <= 10; i++) 
+                {
+                    Console.Write("{0}*{1}={2}\t", i, j, (i * j)); 
+                }
+                Console.WriteLine(); 
+            }
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+        }
+    }
+}
